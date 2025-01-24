b2cfdc300d0f8ecff31cc8bfebd6b8750787e66f
maruishi <ayaka0622.sakura@gmail.com>
2025-01-25 01:50:11 +0900

    test2

--- frontend/src/pages/dashboard.js
+++ frontend/src/pages/dashboard.js
@@ -1,7 +1,28 @@
 import AppLayout from '@/components/Layouts/AppLayout'
+import MovieList from '@/components/MovieList'
 import Head from 'next/head'
+import { useEffect, useState } from 'react'
 
 const Dashboard = () => {
+    const [movies, setMovies] = useState([])
+
+    useEffect(() => {
+        const fetchMovies = async () => {
+            try {
+                const response = await fetch('api/getNowPlayingMovies')
+                if (!response.ok) {
+                    throw new Error('Network response was not ok')
+                }
+                const data = await response.json()
+                setMovies(data.results)
+                console.log(data)
+            } catch (error) {
+                console.log(error)
+            }
+        }
+        fetchMovies()
+    }, [])
+
     return (
         <AppLayout
             header={
@@ -10,18 +31,10 @@ const Dashboard = () => {
                 </h2>
             }>
             <Head>
-                <title>Laravel - Dashboard</title>
+                <title>Dashboard - CinemaLoveReview</title>
             </Head>
 
-            {/* <div className="py-12">
-                <div className="max-w-7xl mx-auto sm:px-6 lg:px-8">
-                    <div className="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                        <div className="p-6 bg-white border-b border-gray-200">
-                            You're logged in!
-                        </div>
-                    </div>
-                </div>
-            </div> */}
+            <MovieList title="上映中の映画" movies={movies} />
         </AppLayout>
     )
 }
